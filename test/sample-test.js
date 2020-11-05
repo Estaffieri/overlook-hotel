251060bef1dd6b5419ff2e30e842b1ec854c9708
Estelle Staffieri <estellelareinne@gmail.com>
2020-11-05 15:22:50 -0700

    Remove extra files and add new files to begin TDD

--- test/sample-test.js
+++ test/sample-test.js
@@ -1,5 +1,6 @@
-import chai from 'chai';
-const expect = chai.expect;
+import {expect} from 'chai';
+import User from "../src/classes/User";
+import {sampleUserData} from "../src/data/test-sample-data";
 
 describe('See if the tests are running', function() {
   it('should return true', function() {
