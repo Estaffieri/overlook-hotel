6bdef04de7b6bb31f3b042175422e0f56e8c10d2
Estelle Staffieri <estellelareinne@gmail.com>
2020-11-07 18:16:12 -0700

    Add edge case testing for customer login

--- test/login-customer-test.js
+++ test/login-customer-test.js
@@ -8,8 +8,8 @@ import User from "../src/classes/User.js";
 describe("Customer login validation", function () {
     
   it("these customer credentials should succeed to login", function() {
-    let userRepository = {findByUsername: function (username) {
-        return new User(1 , "Letha");
+    let userRepository = {findByUsername (username) {
+      return new User(1, "Letha");
     } };
     let credentials = {username: "customer1", password: "overlook2020"};
     let loginService = new LoginService(userRepository);
@@ -17,4 +17,28 @@ describe("Customer login validation", function () {
     expect(didLogInSucceed).to.be.equal(true);
   }
   );
+
+  it("if customer doesn't exist they should fail to login", function () {
+    let userRepository = {
+      findByUsername (username) {
+        return null
+      },
+    };
+    let credentials = { username: "customer1", password: "overlook2020" };
+    let loginService = new LoginService(userRepository);
+    let didLogInSucceed = loginService.login(credentials);
+    expect(didLogInSucceed).to.be.equal(false);
+  });
+
+  it("if customer enters wrong password they should fail to login", function () {
+    let userRepository = {
+      findByUsername(username) {
+        return new User(1, "Letha");
+      },
+    };
+    let credentials = { username: "customer1", password: "BANANA" };
+    let loginService = new LoginService(userRepository);
+    let didLogInSucceed = loginService.login(credentials);
+    expect(didLogInSucceed).to.be.equal(false);
+  });
 });
