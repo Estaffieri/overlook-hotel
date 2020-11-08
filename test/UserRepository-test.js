e4ae728f976f80b51b7adfd7dc909adefd7ccce2
Estelle Staffieri <estellelareinne@gmail.com>
2020-11-08 12:13:10 -0700

    Add login-view querySelectors and test findByUsername

--- test/UserRepository-test.js
+++ test/UserRepository-test.js
@@ -2,12 +2,17 @@ import { expect } from "chai";
 import UserRepository from "../src/classes/UserRepository.js";
 import sampleUserData from "../src/data/test-sample-data.js";
 
-describe("Testing stuff and things", function () {
+describe("User Repository", function () {
     
-  it("Good god there is something that needs to work here", function() {
-    console.log(sampleUserData);
+  it("If a customer exists and enters the username correctly they should succeed to login", function() {
     let userRepository = new UserRepository(sampleUserData);
     let user = userRepository.findByUsername("customer2");
     expect(user.id).to.be.equal(2);
   });
+
+it("findByUsername should return the correct user information", function () {
+    let userRepository = new UserRepository([{id:3 , name: "Estelle Staffieri"}]);
+    let user = userRepository.findByUsername("customer3");
+    expect(user.id).to.be.equal(3);
+});
 });
