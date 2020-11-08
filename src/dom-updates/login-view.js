97630d5affd419d132b69f8a461b56bd3dc8c7b3
Estelle Staffieri <estellelareinne@gmail.com>
2020-11-08 12:15:00 -0700

    fix spacing in formUsernameInput

---
+++ src/dom-updates/login-view.js
@@ -0,0 +1,6 @@
+const formErrorMessageSection = document.querySelector(".login-error-message hide");
+const formUsernameInput = document.querySelector(".username-input");
+const formPasswordInput = document.querySelector(".password-input");
+const formLoginButton = document.querySelector("login-button");
+
+
