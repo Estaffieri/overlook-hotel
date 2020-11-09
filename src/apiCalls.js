2c2197bd78a0ee26e3e4361721582665344b5880
Estelle Staffieri <estellelareinne@gmail.com>
2020-11-08 23:37:04 -0700

    Add POST to apiCalls file

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -24,6 +24,24 @@ let apiCalls = {
       .then((data) => data.bookings)
       .catch((error) => console.log(error));
   },
-};
+
+  postNewBooking(booking) {
+    fetch("https://fe-apps.herokuapp.com/api/v1/overlook/1904/bookings/bookings", {
+    method: "POST",
+    headers: {
+        "Content-Type" : "application/json"
+    },
+    body: JSON.stringify({     
+    "userID": booking.userId,
+    "date": booking.date,
+    "roomNumber": booking.roomNumber
+    })
+  })
+
+    .then(response => response.json())
+    .then(data => console.log(data))
+    .then(error => console.log(error))
+    }
+}
 
 export default apiCalls
